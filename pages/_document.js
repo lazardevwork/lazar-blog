63e5b3abf82395348a4c967beae9edd37b8b5d29
lazardevwork/ <lazarledjanac.io@gmail.com>
2022-07-20 00:24:47 +0000

    add the tags

--- pages/_document.js
+++ pages/_document.js
@@ -11,6 +11,21 @@ class MyDocument extends Document {
         >
           <Main />
           <NextScript />
+
+          <script
+            async
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2300992621659894"
+            crossorigin="anonymous"
+          ></script>
+          <ins
+            class="adsbygoogle"
+            style="display:block"
+            data-ad-client="ca-pub-2300992621659894"
+            data-ad-slot="3122840172"
+            data-ad-format="auto"
+            data-full-width-responsive="true"
+          ></ins>
+          <script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
         </body>
         <script
           async
